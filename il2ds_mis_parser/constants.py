9f5fcdafb2aceda9ff7916eb4a619764276ca591
Kamyhin <alexygosev@mail.ru>
2014-03-19 12:02:22 +0300

    Refactor sections name and description

--- il2ds_mis_parser/constants.py
+++ il2ds_mis_parser/constants.py
@@ -2,6 +2,8 @@
 """
 il2ds_mis_parser constants.
 """
+from il2ds_mis_parser.helpers import _
+
 
 # -----------------------------------------------------------------------------
 # All section code
@@ -30,20 +32,20 @@ FRONT_MARKER = "FrontMarker"
 # -----------------------------------------------------------------------------
 
 SECTION = {
-    MAIN: None,
-    SEASON: None,
-    WEATHER: None,
+    MAIN: (_("Main"), _("Basic settings mission"),),
+    SEASON: (_("Season"), _("Date mission"),),
+    WEATHER: (_("Weather"), _("Weather conditions"),),
     MDS: None,
-    RESPAWN_TIME: None,
+    RESPAWN_TIME: (_("Time respawn objects"), None,),
     CHIEFS: None,
     CHIEF_ROAD: None,
     N_STATIONARY: None,
-    BUILDINGS: None,
-    TARGET: None,
-    BORN_PLACE: None,
-    BORN_PLACE_COUNTRIES: None,
-    STATIC_CAMERA: None,
-    BRIDGE : None,
+    BUILDINGS: (_("Building"), None,),
+    TARGET: (_("Target"), None,),
+    BORN_PLACE: (_("Airfield"), None),
+    BORN_PLACE_COUNTRIES: (_("Airfield country"), None),
+    STATIC_CAMERA: (_("Static camera"), None,),
+    BRIDGE : (_("Bridge"), None,),
     HOUSE: None,
-    FRONT_MARKER: None,
+    FRONT_MARKER: (_("Front line"), None,),
 }
