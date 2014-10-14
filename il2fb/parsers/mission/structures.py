1e14c011f99e1ab98fd7a80366cbd25383688638
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-10-14 12:14:31 +0300

    extract base class for data structures

--- il2fb/parsers/mission/structures.py
+++ il2fb/parsers/mission/structures.py
@@ -1,26 +1,34 @@
 # -*- coding: utf-8 -*-
 
 
-class Point2D(object):
+class BaseObject(object):
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return all([
+            getattr(self, x) == getattr(other, x)
+            for x in self.__slots__
+        ])
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __hash__(self):
+        return hash(tuple(
+            getattr(self, x) for x in self.__slots__
+        ))
+
+
+class Point2D(BaseObject):
     __slots__ = ['x', 'y', ]
 
     def __init__(self, x, y):
         self.x = float(x)
         self.y = float(y)
 
-    def __eq__(self, other):
-        if not isinstance(other, Point2D):
-            return NotImplemented
-        return self.x == other.x and self.y == other.y
 
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-
-class Point3D(object):
+class Point3D(BaseObject):
     __slots__ = ['x', 'y', 'z', ]
 
     def __init__(self, x, y, z):
@@ -28,19 +36,8 @@ class Point3D(object):
         self.y = float(y)
         self.z = float(z)
 
-    def __eq__(self, other):
-        if not isinstance(other, Point3D):
-            return NotImplemented
-        return self.x == other.x and self.y == other.y and self.z == other.z
 
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __hash__(self):
-        return hash((self.x, self.y, self.z))
-
-
-class GroundRoutePoint(object):
+class GroundRoutePoint(BaseObject):
     __slots__ = ['pos', 'is_checkpoint', 'delay', 'section_length', 'speed', ]
 
     def __init__(self, pos, is_checkpoint, delay=None, section_length=None,
@@ -51,28 +48,8 @@ class GroundRoutePoint(object):
         self.section_length = section_length
         self.speed = speed
 
-    def __eq__(self, other):
-        if not isinstance(other, GroundRoutePoint):
-            return NotImplemented
-        return (
-            self.pos == other.pos
-            and self.is_checkpoint == other.is_checkpoint
-            and self.delay == other.delay
-            and self.section_length == other.section_length
-            and self.speed == other.speed
-        )
 
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __hash__(self):
-        return hash((
-            self.pos, self.is_checkpoint, self.delay, self.section_length,
-            self.speed,
-        ))
-
-
-class Building(object):
+class Building(BaseObject):
     __slots__ = ['id', 'belligerent', 'code', 'pos', 'rotation_angle', ]
 
     def __init__(self, id, belligerent, code, pos, rotation_angle):
@@ -82,47 +59,16 @@ class Building(object):
         self.pos = pos
         self.rotation_angle = rotation_angle
 
-    def __eq__(self, other):
-        if not isinstance(other, Building):
-            return NotImplemented
-        return (
-            self.id == other.id
-            and self.code == other.code
-            and self.belligerent == other.belligerent
-            and self.pos == other.pos
-            and self.rotation_angle == other.rotation_angle
-        )
 
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __hash__(self):
-        return hash((
-            self.id, self.code, self.belligerent, self.pos,
-            self.rotation_angle,
-        ))
-
-
-class StaticCamera(object):
+class StaticCamera(BaseObject):
     __slots__ = ['belligerent', 'pos', ]
 
     def __init__(self, belligerent, pos):
         self.belligerent = belligerent
         self.pos = pos
 
-    def __eq__(self, other):
-        if not isinstance(other, StaticCamera):
-            return NotImplemented
-        return self.belligerent == other.belligerent and self.pos == other.pos
 
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __hash__(self):
-        return hash((self.belligerent, self.pos))
-
-
-class FrontMarker(object):
+class FrontMarker(BaseObject):
     __slots__ = ['id', 'belligerent', 'pos', ]
 
     def __init__(self, id, belligerent, pos):
@@ -130,23 +76,8 @@ class FrontMarker(object):
         self.belligerent = belligerent
         self.pos = pos
 
-    def __eq__(self, other):
-        if not isinstance(other, FrontMarker):
-            return NotImplemented
-        return (
-            self.id == other.id
-            and self.belligerent == other.belligerent
-            and self.pos == other.pos
-        )
 
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __hash__(self):
-        return hash((self.id, self.belligerent, self.pos))
-
-
-class Rocket(object):
+class Rocket(BaseObject):
     __slots__ = [
         'id', 'code', 'belligerent', 'pos', 'rotation_angle', 'delay', 'count',
         'period', 'destination',
@@ -163,28 +94,3 @@ class Rocket(object):
         self.count = count
         self.period = period
         self.destination = destination
-
-    def __eq__(self, other):
-        if not isinstance(other, Rocket):
-            return NotImplemented
-        return (
-            self.id == other.id
-            and self.code == other.code
-            and self.belligerent == other.belligerent
-            and self.pos == other.pos
-            and self.rotation_angle == other.rotation_angle
-            and self.delay == other.delay
-            and self.count == other.count
-            and self.period == other.period
-            and self.destination == other.destination
-        )
-
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __hash__(self):
-        return hash((
-            self.id, self.code, self.belligerent, self.pos,
-            self.rotation_angle, self.delay, self.count, self.period,
-            self.destination,
-        ))
