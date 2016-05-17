87c1c9b4d1995e5316fe87cd763c96418fa87500
Alexander Oblovatniy <oblovatniy@gmail.com>
2016-05-17 23:18:21 +0300

    tidy up app

---
+++ jsx/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from './components/main.jsx'
+
+ReactDOM.render(<Main />, document.getElementById('main'));
--- jsx/components/main.jsx
+++ jsx/components/main.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import {deepOrange500} from 'material-ui/styles/colors';
-
-
-// Needed for onTouchTap
-// Can go away when react 1.0 release
-// Check this repo:
-// https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin();
 
 
 const styles = {
@@ -31,7 +21,7 @@ const muiTheme = getMuiTheme({
 });
 
 
-class App extends React.Component {
+export default class Main extends React.Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
@@ -39,17 +29,11 @@ class App extends React.Component {
           <h1>material-ui</h1>
           <h2>example project</h2>
           <RaisedButton
-            label="Super Secret Password"
+            label="Hello button"
             primary={true}
-            onTouchTap={this.handleTouchTap}
           />
         </div>
       </MuiThemeProvider>
     );
   }
 }
-
-
-// Render the main app react component into the app div.
-// For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-ReactDOM.render(<App />, document.getElementById('app'));
