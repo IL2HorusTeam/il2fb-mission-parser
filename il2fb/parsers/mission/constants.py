9a853262db138ae8c007106b941172efda328539
Alexander Oblovatniy <oblovatniy@gmail.com>
2016-10-13 21:16:50 +0300

    update docs

--- il2fb/parsers/mission/constants.py
+++ il2fb/parsers/mission/constants.py
@@ -1,10 +1,29 @@
 # coding: utf-8
 
+#: Flag which indicates whether stationary aircraft is restorable in case it
+#: can be used for spawning.
 IS_STATIONARY_AIRCRAFT_RESTORABLE = '2'
+
+#: Flag for empty values.
 NULL = 'null'
+
+#: Flag which indicates new line with continuation of weapon limitations.
 WEAPONS_CONTINUATION_MARK = '+'
+
+#: Flag which indicates new line with extra parameters for flight route points.
 ROUTE_POINT_EXTRA_PARAMETERS_MARK = 'TRIGGERS'
+
+#: Flag which indicates whether radio silence is disabled for a given route
+#: point of flight.
 ROUTE_POINT_RADIO_SILENCE_OFF = '&0'
+
+#: Flag which indicates whether radio silence is enabled for a given route
+#: point of flight.
 ROUTE_POINT_RADIO_SILENCE_ON = '&1'
+
+#: Enumeration of flags which indicate beginning of a comment.
 COMMENT_MARKERS = (';', '#', '//', '--', )
+
+#: Multiplication coefficient which is used to convert speed of moving ground
+#: units into km/h.
 CHIEF_SPEED_COEFFICIENT = 3.6
