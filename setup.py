d0d623cc0d0b33a319f787268f048827d13d6800
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-07-17 22:13:40 +0300

    Update version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ README = open(os.path.join(__here__, 'README.rst')).read()
 
 setup(
     name='il2fb-mission-parser',
-    version='1.1.0.dev3',
+    version='1.1.1',
     description=(
         "Parse IL-2 FB mission file and produce detailed information about "
         "mission."
